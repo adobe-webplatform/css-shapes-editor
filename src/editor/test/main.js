90354b0c352e67f996650bc3dc646e2c0d29acf4
Razvan Caliman <razvan.caliman@gmail.com>
2013-11-11 13:05:02 +0000

    test for target with external markup

--- src/editor/test/main.js
+++ src/editor/test/main.js
@@ -3,11 +3,12 @@ require.config({
     baseUrl: '../',
     paths: {
         'jquery': 'third-party/jquery/jquery.min',
+        'text': 'third-party/requirejs/text',
         'eve': 'third-party/eve/eve',
         'spec': 'test/spec'
     }
 });
-require(['spec/CSSShapesEditorSpec'], function(){
+require(['text','spec/CSSShapesEditorSpec'], function(){
     var env = jasmine.getEnv();
     env.addReporter(new jasmine.HtmlReporter);
     env.execute();
--- src/editor/test/spec/CSSShapesEditorSpec.js
+++ src/editor/test/spec/CSSShapesEditorSpec.js
@@ -2,38 +2,49 @@
 /*global define, describe, it, expect, beforeEach, afterEach, waits, waitsFor, runs, $, waitsForDone, spyOn */
 
 // see main.js for path mapping config
-define(['jquery','CSSShapesEditor','PolygonEditor','CircleEditor'],
-function($, CSSShapesEditor, PolygonEditor, CircleEditor){
+define(['jquery', 'text!spec/test-files/polygon.html', 'CSSShapesEditor', 'PolygonEditor', 'CircleEditor'],
+function($, markup, CSSShapesEditor, PolygonEditor, CircleEditor){
     
     describe('CSSShapesEditor', function(){
-        var target, property, value, editor;
-
+        var editor, 
+            target, 
+            property = 'shape-inside',
+            value = 'polygon(nonzero, 0 0, 100px 0, 100px 100px)',
+            $fixture = $('#test-fixture').html(markup);
+            // target = $('#test-shape')[0]
+            
         beforeEach(function(){
-            target = $('#shape-element');
-            property = 'shape-inside';
-            value = '';
+            // inject markup for test
+            $fixture.html(markup)
+            target = $('#test-shape')[0]
+        })
+        
+        afterEach(function(){
+            // clean-up markup
+            $fixture.empty()
         })
 
         it('should be defined', function(){
-            value = 'circle(50%, 50%, 50%)';
             editor = new CSSShapesEditor(target, property, value);
             expect(editor).toBeDefined();
         });
 
         it('should return instance of polygon editor', function(){
-            value = 'polygon(nonzero, 0 0, 100px 0, 100px 100px)';
+            var value = 'polygon(nonzero, 0 0, 100px 0, 100px 100px)';
+            
             editor = new CSSShapesEditor(target, property, value);
             expect(editor instanceof PolygonEditor).toBe(true);
         });
 
         it('should return instance of circle editor', function(){
-            value = 'circle(50%, 50%, 50%)';
+            var value = 'circle(50%, 50%, 50%)';
+            
             editor = new CSSShapesEditor(target, property, value);
             expect(editor instanceof CircleEditor).toBe(true);
         });
 
         it('should throw error for unknown shape in value', function(){
-            value = 'fake-shape()';
+            var value = 'fake-shape()';
 
             var setup = function() {
                 editor = new CSSShapesEditor(target, property, value);
@@ -41,7 +52,7 @@ function($, CSSShapesEditor, PolygonEditor, CircleEditor){
 
             expect(setup).toThrow();
         });
-
+        
         it('should throw error for invalid value', function(){
             var setupWithUndefined = function() {
                 editor = new CSSShapesEditor(target, property, undefined);
@@ -59,6 +70,30 @@ function($, CSSShapesEditor, PolygonEditor, CircleEditor){
                 editor = new CSSShapesEditor(target, property, 0);
             };
 
+            expect(setupWithUndefined).toThrow();
+            expect(setupWithNull).toThrow();
+            expect(setupWithEmpty).toThrow();
+            expect(setupWithZero).toThrow();
+        })
+
+        it('should throw error for invalid target', function(){
+            var setupWithUndefined = function() {
+                editor = new CSSShapesEditor(undefined, property, value);
+            };
+
+            var setupWithNull = function() {
+                editor = new CSSShapesEditor(null, property, value);
+            };
+
+            var setupWithEmpty = function() {
+                editor = new CSSShapesEditor('', property, value);
+            };
+
+            var setupWithZero = function() {
+                editor = new CSSShapesEditor(0, property, value);
+            }; 
+            
+            
             expect(setupWithUndefined).toThrow();
             expect(setupWithNull).toThrow();
             expect(setupWithEmpty).toThrow();
