61d5a15d6b35d6bc00dc33eeae7a0767e248f422
Razvan Caliman <razvan.caliman@gmail.com>
2013-11-12 14:37:49 +0000

    todo load raphael shim

--- src/editor/main.js
+++ src/editor/main.js
@@ -4,6 +4,8 @@ require.config({
         app: '../app',
         'eve': 'third-party/eve/eve'
     }
+    
+    // TODO: add Raphael as shim
 });
 
 define('main', ['CSSShapesEditor'], function(editor){
