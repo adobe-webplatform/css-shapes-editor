b73e3b349a45e371fdb74e8a9ed315a93e0934dc
Razvan Caliman <razvan.caliman@gmail.com>
2014-03-16 14:33:08 +0000

    remove mutation logic on update()

--- src/CSSShapesEditor.js
+++ src/CSSShapesEditor.js
@@ -2,11 +2,11 @@
 /*global define */
 
 define(['PolygonEditor', 'CircleEditor', 'EllipseEditor', 'RectangleEditor', 'lodash'], function(PolygonEditor, CircleEditor, EllipseEditor, RectangleEditor, _){
-    
+
     'use strict';
-    
+
     function CSSShapesEditor(target, value, options){
-        
+
         var _defaults = {
             // multiple objects with attributes are used into separate <use> objects for the shape path
             path: [
@@ -30,12 +30,12 @@ define(['PolygonEditor', 'CircleEditor', 'EllipseEditor', 'RectangleEditor', 'lo
                 opacity: 0.8
             }
         };
-        
+
         options = _.extend({}, _defaults, options);
-        
+
         /*
             Get shape type from provided string.
-            
+
             @param {String} string with function-like notation such as:
                             polygon(...), circle(), ellipse() or rectangle()
             @throws {TypeError} if input does not contain function-like notation
@@ -49,14 +49,14 @@ define(['PolygonEditor', 'CircleEditor', 'EllipseEditor', 'RectangleEditor', 'lo
         }
         /*
             Get shape editor class appropriate for given shape.
-            
+
             @param {String} shape Any of: polygon, circle, ellipse, rectangle
             @throws {TypeError} if shape is not recognized
             @return {Object} shape editor class
         */
         function _getFactory(shape){
             var factory;
-            
+
             switch (shape) {
             case 'polygon':
                 factory = PolygonEditor;
@@ -77,62 +77,29 @@ define(['PolygonEditor', 'CircleEditor', 'EllipseEditor', 'RectangleEditor', 'lo
             default:
                 throw new TypeError('Value does not contain a valid shape function');
             }
-            
+
             return factory;
         }
-        
+
         // ensure omitting 'new' is harmless
         if (!(this instanceof CSSShapesEditor)){
             return new CSSShapesEditor(target, value, options);
         }
-        
+
         if (!(target instanceof HTMLElement)){
             throw new TypeError('Target is not instance of HTMLElement');
         }
-        
+
         if (!value || typeof value !== 'string'){
             throw new TypeError('Value is not string');
         }
-        
+
         var _shape = _getShape(value),
             Factory = _getFactory(_shape),
-            _old_update = Factory.prototype.update,
             _editor = new Factory(target, value, options);
-        
-        /* 
-            Hijack the update method to check if shape editor needs 
-            to mutate when the update string contains a different shape type.
-            
-            @example 'polygon()' -> 'circle()', editor becomes CircleEditor instance
-        */
-        Factory.prototype.update = function(value){
-            
-            var newShape = _getShape(value);
-            
-            // updating to a different shape type, replace the editor.
-            if (newShape !== _shape){
-                
-                Factory = _getFactory(newShape);
-                
-                // clean-up old editor
-                _editor.remove();
-                
-                // cache current shape type
-                _shape = newShape;
-                
-                // replace editor
-                _editor = new Factory(target, value, options);
-                
-                return;
-            }
-            // updating same shape type; business as usual
-            else{
-                _old_update.call(_editor, value);
-            }
-        };
         
         return _editor;
     }
-    
+
     return CSSShapesEditor;
 });
